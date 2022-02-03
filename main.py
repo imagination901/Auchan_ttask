1a870dfe73e39cd1deef1444f8b72a1a596cafcf
Avor Sky <1737000@gmail.com>
2022-02-03 14:09:50 +0200

    Added URL parsing functionality as per the 2-nd requirement

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ data = []
 
 with open("messages_to_parse.dat", 'rb') as file:
     data = pickle.load(file)
+
+
+# 2. Parsing the data to find all existing URLs from the extracted data
+
+urls = []
+url_extractor = URLExtract()
+
+for string in data:
+    url = url_extractor.find_urls(string)
+    if url:
+        urls.append(url)
+
+for i, url in enumerate(urls):
+    print(i, url)
